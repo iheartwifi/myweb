ca913d023e3c712e7f1e53f2c057d023cdc0f593
iheartwifi <wanlanhe@gmail.com>
2017-01-31 20:34:40 -0500

    Add files via upload

---
+++ portfolio.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>In Code We Trust</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                
+                <h1 class="page-header"><br>Portfolio
+                    <small>Projects</small>
+                </h1>
+            </div>
+        </div>
+        <!-- row -->
+
+        <!-- Project One -->
+        <div class="row">
+            <div class="col-md-5 col-md-offset-2">
+                <a href="#">
+                    <img class="img-responsive" src="images/hangman.jpg" alt="project#1">
+
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Project One</h3>
+                <h4>Hangman</h4>
+                <p>Simulation of Hangman as a part of project work in java
+
+</p>
+                <a class="btn btn-primary" href="https://github.com/iheartwifi/Hangman">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- row -->
+        <hr>
+
+        <!-- Project Two -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="images/lnf.jpg" alt="project#2">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Project Two</h3>
+                <h4>Lost & Found </h4>
+                <p>Information center for lost and found items written in Java</p>
+                <a class="btn btn-primary" href="https://github.com/iheartwifi/LostAndFound/tree/master/ObjetsPerdu3">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!-- row -->
+
+        <hr>
+        
+        <!-- Project Three -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="images/book.jpg" alt="project#3">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Project Three</h3>
+                <h4>Publication Book</h4>
+                <p>Program prompts with the user to enter the name of the output file that will be created to hold the modified/correct publishing book inventory.</p>
+                <a class="btn btn-primary" href="https://github.com/iheartwifi/PublicationBook">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
+        </div>
+        <!--row -->
+
+        <hr>
+
+        
+        <!-- Project Four -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="images/web.jpg" alt="project#4">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Project Four</h3>
+                <h4>Website</h4>
+                <p>Responsive WebPages using HTLM5, CSS3, PHP, JavaScript, JQuery, BootStrap</p>
+                <a class="btn btn-primary" href="">Coming soon</a>
+            </div>
+        </div>
+        <!-- row -->
+
+        <hr>
+        
+        
+        <!-- Project Five -->
+        <div class="row">
+            <div class="col-md-7">
+                <a href="#">
+                    <img class="img-responsive" src="images/stock.jpg" alt="project#5">
+                </a>
+            </div>
+            <div class="col-md-5">
+                <h3>Project Five</h3>
+                <h4>Simplified version of a stock market </h4>
+                <p>Create an order book to record the interest of buyers and sellers and matches the prices through a matching engine</p>
+                <a class="btn btn-primary" href="https://github.com/iheartwifi/SimplifiedStockMarket">View Project</a>
+            </div>
+        </div>
+        <!-- row -->
+
+        <hr>
+        
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                            <p>Copyright &copy; 2017<a href="index.html"> Wan Lan He</a></p>
+                </div>
+            </div>
+            <!-- row -->
+        </footer>
+
+    </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+        //Disable full page
+        $("body").on("contextmenu",function(e){
+            return false;
+            });
+        });
+    </script>
+    
+
+</body>
+
+</html>
